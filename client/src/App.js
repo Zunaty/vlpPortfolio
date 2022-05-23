6b7bc7d381ad01bdf6cc93ea2f42414d9b966206
Victor Perez <Zunaty@gmail.com>
2022-05-23 10:04:06 -0600

    working on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,71 @@
-import react from 'react';
+// Importing React
+import react, { useState } from 'react';
+
+// Importing MUI components
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+// Importing components / pages
+import Nav from './components/navbar';
+import Footer from './components/footer';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+// MUI Theme and light / dark
+const lightTheme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: ''
+    },
+    secondary: {
+      main: ''
+    },
+    success: {
+      main: ''
+    },
+    info: {
+      main: ''
+    }
+  }
+});
+
+const darkTheme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: ''
+    },
+    secondary: {
+      main: ''
+    },
+    success: {
+      main: ''
+    },
+    info: {
+      main: ''
+    }
+  }
+})
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
 
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+
+  return (
+    <div>
+      <Nav></Nav>
+
+      <main>
+
+      </main>
+
+      <Footer></Footer>
+    </div>
+  );
+};
 
 export default App;
