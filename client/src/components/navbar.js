bd2fef92f41d39fa7db74a7917f8bfb3c76cac36
Victor Perez <Zunaty@gmail.com>
2022-05-24 13:31:13 -0600

    sign up page work

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,54 +2,47 @@
 import * as React from 'react';
 
 // Importing MUI
-import { 
-    AppBar,
-    Box,
-    Toolbar,
-    Typography,
-    Button,
-    IconButton
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
 } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu'
 
 function Nav(props) {
-    console.log(props);
+  console.log(props);
 
-    return (
-        <Box
+  return (
+    <Box
+      sx={{ flexGrow: 1 }}
+    >
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography
+            variant='h6'
+            component='div'
             sx={{ flexGrow: 1 }}
-        >
-            <AppBar position='static'>
-                <Toolbar>
-                    <Typography
-                        variant='h6'
-                        component='div'
-                        sx={{ flexGrow: 1 }}
-                    >
-                        Victor L Perez
-                    </Typography>
+          >
+            Victor L Perez
+          </Typography>
 
-                    <Button
-                        color='inherit'
-                    >
-                        Login
-                    </Button>
-
-                    <IconButton
-                        size='large'
-                        edge='start'
-                        color='inherit'
-                        aria-label='menu'
-                        sx={{ ml: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            
-        </Box>
-    )
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ ml: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
 }
 
 export default Nav;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,142 @@
+// Importing React
 import * as React from 'react';
 
+// Importing MUI
+import {
+  Container,
+  Box,
+  Typography,
+  Grid,
+  TextField,
+} from '@mui/material'
+
 function Signup() {
-    return (
-        <div>
-            Signup Page
-        </div>
-    )
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password')
+    });
+  };
+
+  return (
+    <Container
+      component='main'
+      maxWidth='xs'
+    >
+      {/* Form box */}
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        {/* Sign up title */}
+        <Typography
+          component='h1'
+          variant='h5'
+        >
+          Sign up
+        </Typography>
+
+        {/* Form box */}
+        <Box
+          component='form'
+          noValidate
+          onSubmit={handleSubmit}
+          sx={{ mt: 3 }}
+        >
+          <Grid
+            container
+            spacing={2}
+          >
+            {/* First name grid item */}
+            <Grid
+              item
+              xs={12}
+              sm={6}
+            >
+              <TextField
+                id='firstName'
+                name='firstName'
+                label='First Name'
+                autoComplete='given-name'
+                required
+                fullWidth
+                autoFocus
+              />
+            </Grid>
+
+            {/* Last name grid item */}
+            <Grid
+              item
+              xs={12}
+              sm={6}
+            >
+              <TextField
+                id='lastName'
+                name='lastName'
+                label='Last Name'
+                autoComplete='family-name'
+                required
+                fullWidth
+              />
+            </Grid>
+
+            {/* Email grid item */}
+            <Grid
+              item
+              xs={12}
+            >
+              <TextField
+                id='email'
+                name='email'
+                label='Email'
+                autoComplete='email'
+                required
+                fullWidth
+              />
+            </Grid>
+
+            {/* Username grid item */}
+            <Grid
+              item
+              xs={12}
+            >
+              <TextField
+                id='username'
+                name='username'
+                label='Username'
+                autoComplete='username'
+                required
+                fullWidth
+              />
+            </Grid>
+
+            {/* Password grid item */}
+            <Grid
+              item
+              xs={12}
+            >
+              <TextField
+                id='password'
+                name='password'
+                label='Password'
+                autoComplete='new-password'
+                type='password'
+                required
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Signup;
